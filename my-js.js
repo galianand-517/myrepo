fb95822672d398ab84e32f9048e9368abe8be241
galianand-517 <galianand74@gmail.com>
2025-05-19 14:18:52 +0530

    Update my-js.js

--- my-js.js
+++ my-js.js
@@ -1,41 +1,32 @@
-const crypto = require('crypto');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
-// Generate a 32-byte key (256 bits) for AES-256
-const secretKey = crypto.randomBytes(32); // <-- Run this once and store securely
-console.log("Secret Key (Base64):", secretKey.toString('base64'));
 
-// Simulate a saved secret key
-const key = Buffer.from('0123456789abcdef0123456789abcdef', 'utf-8'); // 32-byte key
+const secretKey = "mySuperSecretKey123";
 
-// Initialization vector (should be random for each encryption)
-const iv = crypto.randomBytes(16);
 
-// Sensitive data
-const sensitiveData = "my_api_key_123456789";
+http.createServer(function (req, res) {
+    const queryObject = url.parse(req.url, true).query;
 
-// Encrypt function
-function encrypt(data, key, iv) {
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-    let encrypted = cipher.update(data, 'utf8', 'base64');
-    encrypted += cipher.final('base64');
-    return {
-        iv: iv.toString('base64'),
-        encryptedData: encrypted
-    };
-}
+   
+    if (queryObject.name) {
+        res.write(`<h1>Hello ${queryObject.name}</h1>`);
+    }
 
-// Decrypt function
-function decrypt(encryptedData, key, iv) {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-    let decrypted = decipher.update(encryptedData, 'base64', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
-}
+   
+    if (queryObject.file) {
+        fs.readFile(queryObject.file, (err, data) => {
+            if (err) {
+                res.write("File not found!");
+            } else {
+                res.write(data);
+            }
+            res.end();
+        });
+    } else {
+        res.end("No file requested.");
+    }
+}).listen(8080);
 
-// Encrypt
-const encrypted = encrypt(sensitiveData, key, iv);
-console.log("Encrypted:", encrypted.encryptedData);
-
-// Decrypt
-const decrypted = decrypt(encrypted.encryptedData, key, Buffer.from(encrypted.iv, 'base64'));
-console.log("Decrypted:", decrypted);
+console.log("Server running at http://localhost:8080/");
